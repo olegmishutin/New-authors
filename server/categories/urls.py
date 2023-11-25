df1f7a2a8efcac928f7118080df177a340ca1540
Oleg Mishutin <omisutin646@gmail.com>
2023-11-25 14:27:52 +0300

    added categories page form admin

--- server/categories/urls.py
+++ server/categories/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'categories'
 urlpatterns = [
     path('categories/', views.categories, name='categories'),
+    path('categories-admin/', views.categoriesAdmin, name='categories-admin'),
     path('creating-category/', views.creatingCategory, name='creating-category')
 ]
--- server/categories/views.py
+++ server/categories/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 
 def categories(request):
-    return render(request, 'categories/categories.html')
+    return render(request, 'categories/categories.html', {'isAdmin': False})
+
+
+def categoriesAdmin(request):
+    return render(request, 'categories/categories.html', {'isAdmin': True})
+
 
 def creatingCategory(request):
     return render(request, 'categories/creating category.html')
