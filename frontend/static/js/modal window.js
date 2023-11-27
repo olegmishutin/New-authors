99f87b51cf87d3e98b8015db329502c53c1a5f78
Oleg Mishutin <omisutin646@gmail.com>
2023-11-27 23:20:04 +0300

    fixed

--- frontend/static/js/modal window.js
+++ frontend/static/js/modal window.js
@@ -1,20 +1,17 @@
 const modalWindowBox = document.getElementById('modal-window-box')
-const modalWindow = document.getElementById('model-window')
-
 const openModalwindowButtons = document.getElementsByClassName('call-modal-window')
 
 for (let i = 0; i < openModalwindowButtons.length; i++) {
     openModalwindowButtons[i].onclick = function () {
-        modalWindowBox.className = 'modal-window-box content-padding show-modal-window-box'
-        modalWindow.className = 'modal-window open-window-animation'
+        modalWindowBox.className = 'modal-window-box content-padding open-modal-window-box-animation show-modal-window-box'
     }
 }
 
 document.getElementById('close-model-window').onclick = function () {
-    modalWindow.className = 'modal-window close-window-animation'
+    modalWindowBox.className = 'modal-window-box content-padding close-modal-window-box-animation show-modal-window-box'
 
-    modalWindow.addEventListener('animationend', function listener(e) {
+    modalWindowBox.addEventListener('animationend', function listener(e) {
         modalWindowBox.className = 'modal-window-box content-padding'
-        modalWindow.removeEventListener(e.type, listener)
+        modalWindowBox.removeEventListener(e.type, listener)
     })
 }
