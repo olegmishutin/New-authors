3b87047335a8caf4d577f00f7f759db426960e3b
Oleg Mishutin <omisutin646@gmail.com>
2023-12-10 22:29:56 +0300

    added backend for menu, fixed

--- server/New_authors/views.py
+++ server/New_authors/views.py
@@ -1,5 +1,18 @@
+import random
 from django.shortcuts import render
+from users.models import User
+from books.models import Book
+from categories.models import Category
 
 
 def menu(request):
-    return render(request, 'menu.html')
+    popularAuthor = User.getAllWithStatistics(User).order_by('-reviewsCount')[:12]
+    books = Book.getAllWithStatistics(Book)
+
+    newBooks = books.order_by('-publication_date')[:12]
+    popularBooks = books.order_by('-reviewsCount')
+
+    randomCategory = random.choice(Category.objects.all())
+    return render(request, 'menu.html',
+                  {'popularAuthor': popularAuthor, 'newBooks': newBooks, 'popularBooks': popularBooks,
+                   'category': randomCategory})
